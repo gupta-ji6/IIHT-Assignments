23d9211cd56998f25a7e85302df9c02fdf52b909
cogvilt23@iiht.tech <cogvilt23@iiht.tech>
2019-07-27 10:35:23 +0530

    :heavy_check_mark: Added Swap

---
+++ July26/Swap.java
@@ -0,0 +1,16 @@
+package com.assignment.july26;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int temp;
+		System.out.println("Before swapping: a = "+ a + " and b = " + b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After swapping: a = "+ a + " and b = " + b);
+	}
+}
