f201385aefb4904fe187553e0572913501a67330
cogvilt23@iiht.tech <cogvilt23@iiht.tech>
2019-07-27 10:19:13 +0530

    :heavy_check_mark: Added HelloWorld

---
+++ July26/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.assignment.july26;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
