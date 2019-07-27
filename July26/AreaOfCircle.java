fc6b9cb609e47cf1a9a7a9cea6edbb34ccb1daa6
cogvilt23@iiht.tech <cogvilt23@iiht.tech>
2019-07-27 10:32:32 +0530

    :heavy_check_mark: Added AreaOfCircle

---
+++ July26/AreaOfCircle.java
@@ -0,0 +1,8 @@
+package com.assignment.july26;
+
+public class AreaOfCircle {
+	public static void main(String args[]) {
+		int r = 2;
+		System.out.println("Area of circle of radius " + r + " is " + 3.14*r*r);
+	}
+}
