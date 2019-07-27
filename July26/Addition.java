cbcdb820083600291e4fbf8c7163626402db5488
cogvilt23@iiht.tech <cogvilt23@iiht.tech>
2019-07-27 10:31:44 +0530

    :heavy_check_mark: Added Addition

---
+++ July26/Addition.java
@@ -0,0 +1,12 @@
+package com.assignment.july26;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 30;
+		int c = a+b;
+		System.out.println("Sum of " + a + " and " + b + " is equal to " + c);
+	}
+
+}
