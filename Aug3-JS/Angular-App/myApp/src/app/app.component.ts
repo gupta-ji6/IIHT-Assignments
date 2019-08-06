4f9d10dd4b2f60837addb5ad0bba3c8a14c249a1
gupta-ji6 <ayushgupta197@gmail.com>
2019-08-06 09:29:51 +0530

    :heavy_check_mark: Added Angular App

---
+++ Aug3-JS/Angular-App/myApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myApp';
+  message = "Kashmir hamara hai!";
+}
