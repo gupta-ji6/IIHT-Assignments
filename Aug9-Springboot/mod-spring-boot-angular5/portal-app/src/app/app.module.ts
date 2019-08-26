8a6ea07f0a987dec7a9d2ccf93ee965e4f43f9d1
gupta-ji6 <ayushgupta197@gmail.com>
2019-08-26 12:50:32 +0530

    :art: Updated sidenav text color & removed ongoing

--- Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/app.module.ts
+++ Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/app.module.ts
@@ -1,10 +1,28 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule, MatDatepickerModule, MatSliderModule, MatChipsModule,MatProgressBarModule,MatCheckboxModule, MatPaginatorModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule, MatCardModule,MatMenuModule, MatIconModule, MatSidenavModule, MatListModule, MatSelectModule, MatSortModule} from '@angular/material';
-import {LayoutModule} from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatSliderModule,
+  MatChipsModule,
+  MatProgressBarModule,
+  MatCheckboxModule,
+  MatPaginatorModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatSelectModule,
+  MatSortModule
+} from "@angular/material";
+import { LayoutModule } from "@angular/cdk/layout";
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user/user.component";
 import { AppRoutingModule } from "./app.routing.module";
@@ -15,15 +33,18 @@ import { LoginComponent } from "./components/login/login.component";
 import { SignUpComponent } from "./components/user/sign-up/sign-up.component";
 import { UserLandingComponent } from "./components/user/user-landing/user-landing.component";
 import { CompletedTrainingComponent } from "./components/user/completed-training/completed-training.component";
-import { OngoingTrainingComponent } from "./components/user/ongoing-training/ongoing-training.component";
-import {UserPaymentsComponent} from './components/user/user-payments/user-payments.component';
-import {HomeComponent} from './components/home/home.component';
-import {SearchTrainingsComponent} from './components/user/search-trainings/search-trainings.component'
-import {MentorCompletedTrainingsComponent} from './components/mentor/mentor-completed-trainings/mentor-completed-trainings.component'
-import {MentorSignUpComponent} from './components/mentor/mentor-sign-up/mentor-sign-up.component';
-import { MentorProfileComponent } from './components/mentor/mentor-profile/mentor-profile.component';
-import {AdminEditTechComponent} from './components/admin/admin-edit-tech/admin-edit-tech.component'
-import {AdminUserListComponent} from './components/admin/admin-user-list/admin-user-list.component';
+import { OngoingTrainingComponent } from "./components/user/current-training/ongoing-training.component";
+import { UserPaymentsComponent } from "./components/user/user-payments/user-payments.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchTrainingsComponent } from "./components/user/search-trainings/search-trainings.component";
+import { MentorLandingComponent } from "./components/mentor/mentor-landing/mentor-landing.component";
+import { MentorCurrentTrainingsComponent } from "./components/mentor/mentor-current-trainings/mentor-current-trainings.component";
+import { MentorCompletedTrainingsComponent } from "./components/mentor/mentor-completed-trainings/mentor-completed-trainings.component";
+import { MentorEditSkillsComponent } from "./components/mentor/mentor-edit-skills/mentor-edit-skills.component";
+import { MentorSignUpComponent } from "./components/mentor/mentor-sign-up/mentor-sign-up.component";
+import { MentorProfileComponent } from "./components/mentor/mentor-profile/mentor-profile.component";
+import { AdminEditTechComponent } from "./components/admin/admin-edit-tech/admin-edit-tech.component";
+import { AdminUserListComponent } from "./components/admin/admin-user-list/admin-user-list.component";
 
 @NgModule({
   declarations: [
@@ -38,14 +59,43 @@ import {AdminUserListComponent} from './components/admin/admin-user-list/admin-u
     UserPaymentsComponent,
     HomeComponent,
     SearchTrainingsComponent,
+    MentorLandingComponent,
     MentorSignUpComponent,
     MentorProfileComponent,
+    MentorCurrentTrainingsComponent,
     MentorCompletedTrainingsComponent,
+    MentorEditSkillsComponent,
     AdminEditTechComponent,
     AdminUserListComponent
   ],
-  imports: [LayoutModule, MatDatepickerModule, MatSliderModule, MatChipsModule, MatProgressBarModule,BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, MatPaginatorModule,ReactiveFormsModule,BrowserAnimationsModule,MatTableModule, MatSortModule, MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule, MatSidenavModule, MatListModule, MatSelectModule],
-  providers: [UserService, ],
+  imports: [
+    LayoutModule,
+    MatDatepickerModule,
+    MatSliderModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatPaginatorModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatSelectModule
+  ],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/app.routing.module.ts
+++ Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/app.routing.module.ts
@@ -1,48 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { UserComponent } from './user/user.component';
-import {AddUserComponent} from './user/add-user.component';
-import {LoginComponent} from './components/login/login.component';
-import {SignUpComponent} from './components/user/sign-up/sign-up.component';
-import {UserLandingComponent} from './components/user/user-landing/user-landing.component';
-import {CompletedTrainingComponent} from './components/user/completed-training/completed-training.component';
-import {OngoingTrainingComponent} from './components/user/ongoing-training/ongoing-training.component';
-import {UserPaymentsComponent} from './components/user/user-payments/user-payments.component';
-import {HomeComponent} from './components/home/home.component';
-import {SearchTrainingsComponent} from './components/user/search-trainings/search-trainings.component';
-import {MentorCompletedTrainingsComponent} from './components/mentor/mentor-completed-trainings/mentor-completed-trainings.component'
-import {MentorSignUpComponent} from './components/mentor/mentor-sign-up/mentor-sign-up.component';
-import { MentorProfileComponent } from './components/mentor/mentor-profile/mentor-profile.component';
-import {AdminEditTechComponent} from './components/admin/admin-edit-tech/admin-edit-tech.component'
-import {AdminUserListComponent} from './components/admin/admin-user-list/admin-user-list.component';
+import { UserComponent } from "./user/user.component";
+import { AddUserComponent } from "./user/add-user.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignUpComponent } from "./components/user/sign-up/sign-up.component";
+import { UserLandingComponent } from "./components/user/user-landing/user-landing.component";
+import { CompletedTrainingComponent } from "./components/user/completed-training/completed-training.component";
+import { OngoingTrainingComponent } from "./components/user/current-training/ongoing-training.component";
+import { UserPaymentsComponent } from "./components/user/user-payments/user-payments.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchTrainingsComponent } from "./components/user/search-trainings/search-trainings.component";
+import { MentorLandingComponent } from "./components/mentor/mentor-landing/mentor-landing.component";
+import { MentorCompletedTrainingsComponent } from "./components/mentor/mentor-completed-trainings/mentor-completed-trainings.component";
+import { MentorCurrentTrainingsComponent } from "./components/mentor/mentor-current-trainings/mentor-current-trainings.component";
+import { MentorEditSkillsComponent } from "./components/mentor/mentor-edit-skills/mentor-edit-skills.component";
+import { MentorSignUpComponent } from "./components/mentor/mentor-sign-up/mentor-sign-up.component";
+import { MentorProfileComponent } from "./components/mentor/mentor-profile/mentor-profile.component";
+import { AdminEditTechComponent } from "./components/admin/admin-edit-tech/admin-edit-tech.component";
+import { AdminUserListComponent } from "./components/admin/admin-user-list/admin-user-list.component";
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/home', pathMatch: 'full', },
-  { path: 'users', component: UserComponent },
-  { path: 'add', component: AddUserComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'user/signup', component: SignUpComponent },
-  { path: 'user', component: UserLandingComponent },
-  { path: 'user/completed-trainings', component: CompletedTrainingComponent },
-  { path: 'user/current-trainings', component: OngoingTrainingComponent },
-  { path: 'user/payments', component: UserPaymentsComponent},
-  { path: 'home', component: HomeComponent },
-  {path: 'search', component: SearchTrainingsComponent},
-  {path: 'mentor/signup', component: MentorSignUpComponent},
-  {path: 'mentor/profile', component: MentorProfileComponent},
-  {path: 'mentor/completed-trainings', component: MentorCompletedTrainingsComponent},
-  {path: 'admin/technologies', component: AdminEditTechComponent},
-  {path: 'admin/users', component: AdminUserListComponent},
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "user/signup", component: SignUpComponent },
+  { path: "mentor/signup", component: MentorSignUpComponent },
+  { path: "home", component: HomeComponent },
+  { path: "user", component: UserLandingComponent },
+  { path: "search", component: SearchTrainingsComponent },
+  { path: "user/completed-trainings", component: CompletedTrainingComponent },
+  { path: "user/current-trainings", component: OngoingTrainingComponent },
+  { path: "user/payments", component: UserPaymentsComponent },
+  { path: "mentor", component: MentorLandingComponent },
+  { path: "mentor/profile", component: MentorProfileComponent },
+  { path: "mentor/completed-trainings", component: MentorCompletedTrainingsComponent },
+  { path: "mentor/current-trainings", component: MentorCurrentTrainingsComponent },
+  { path: "mentor/edit-skills", component: MentorEditSkillsComponent },
+  { path: "admin/technologies", component: AdminEditTechComponent },
+  { path: "admin/users", component: AdminUserListComponent },
+  { path: "add", component: AddUserComponent },
+  { path: "users", component: UserComponent }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
