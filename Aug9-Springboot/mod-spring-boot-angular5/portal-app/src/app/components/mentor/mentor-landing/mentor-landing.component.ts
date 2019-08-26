fd45515580f7c7cb5172fecd2d84fdb81f63fa44
gupta-ji6 <ayushgupta197@gmail.com>
2019-08-26 12:52:08 +0530

    :art: Updated mentor landing page

--- Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/components/mentor/mentor-landing/mentor-landing.component.ts
+++ Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/components/mentor/mentor-landing/mentor-landing.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MentorLandingComponent implements OnInit {
 
+  trainingProposals = [
+    {
+      "name" : "Jose Simpson",
+      "profilePic": "https://pbs.twimg.com/profile_images/516590507808419840/V40yR78I.jpeg",
+      "domain": "React",
+      "date": "23-07-2019",
+      "price": 1299,
+    },
+    {
+      "name" : "Alicia Fox",
+      "profilePic": "https://pbs.twimg.com/profile_images/952318165941477376/e-3MyGW3.jpg",
+      "domain": "Tensorflow",
+      "date": "12-07-2019",
+      "price": 2999,
+    },
+    {
+      "name" : "Brian Hemsworth",
+      "profilePic": "https://pbs.twimg.com/profile_images/1079706442763067392/wuaeGZnN.jpg",
+      "domain": "Netlify",
+      "date": "03-09-2019",
+      "price": 999,
+    }
+  ]
+  
   constructor() { }
 
   ngOnInit() {
