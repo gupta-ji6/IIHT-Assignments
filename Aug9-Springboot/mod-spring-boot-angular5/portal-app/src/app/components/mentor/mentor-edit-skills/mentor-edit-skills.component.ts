6a1f053559450183967c4173a6d61a8fe94d81f6
gupta-ji6 <ayushgupta197@gmail.com>
2019-08-26 12:51:53 +0530

    :art: Updated mentor edit skills

--- Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/components/mentor/mentor-edit-skills/mentor-edit-skills.component.ts
+++ Aug9-Springboot/mod-spring-boot-angular5/portal-app/src/app/components/mentor/mentor-edit-skills/mentor-edit-skills.component.ts
@@ -7,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MentorEditSkillsComponent implements OnInit {
 
+  skills = [
+    {
+      "name": "React",
+      "logo": "https://cdn.svgporn.com/logos/react.svg",
+      "domain": "Front End",
+      "proficiency" : "Expert",
+      "numOfTrainings": 32
+    },
+    {
+      "name": "Tensorflow",
+      "logo": "https://cdn.svgporn.com/logos/tensorflow.svg",
+      "domain": "Machine Learning",
+      "proficiency" : "Expert",
+      "numOfTrainings": 49
+    },
+    {
+      "name": "Netlify",
+      "logo": "https://cdn.svgporn.com/logos/netlify.svg",
+      "domain": "Hosting",
+      "proficiency" : "Expert",
+      "numOfTrainings": 15
+    }
+  ];
+
+  domains = [
+    {value: 'Front End', viewValue: 'Front End'},
+    {value: 'Back End', viewValue: 'Back End'},
+    {value: 'Programming Language', viewValue: 'Programming Language'},
+    {value: 'Library', viewValue: 'Library'},
+    {value: 'Database', viewValue: 'Database'},
+    {value: 'Mobile App', viewValue: 'Mobile App'},
+    {value: 'Framework', viewValue: 'Framework'},
+    {value: 'API', viewValue: 'API'}
+  ];
+
+  proficiency = [
+    {
+      value: 'Expert',
+      viewValue: 'Expert'
+    },
+    {
+      value: 'Intermediate',
+      viewValue: 'Intermediate'
+    },
+    {
+      value: 'Amateur',
+      viewValue: 'Amateur'
+    },
+  ]
+  
   constructor() { }
 
   ngOnInit() {
